30128d6b811ba028d38e7f98b5fa4521a8e7dbd4
Amgad Abdullah <amga200d@gmail.com>
2025-02-05 21:06:26 -0700

    refactored allreservations function to fetch data from db

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -99,8 +99,25 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+    SELECT reservations.*, properties.*
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    LIMIT $2;
+  `;
+  const queryParams = [guest_id, limit];
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /// Properties
 
